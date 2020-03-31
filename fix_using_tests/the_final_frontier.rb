e458c72367fc25831c3b9e1b7bf56a1c0956d52d
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-31 01:39:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,14 +7,12 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-star_date = generate_star_date
-
-
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage (date)
+def engage(date)
   puts state_log(date)
   date = generate_star_date
 end
+
